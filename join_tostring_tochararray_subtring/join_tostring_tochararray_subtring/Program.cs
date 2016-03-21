c5d934cdca94d7bf32015f91c0c603dd2da9d68c
iqra1122 <iqra.tariq83@gmail.com>
2016-03-21 18:39:24 +0500

    stringss

---
+++ join_tostring_tochararray_subtring/join_tostring_tochararray_subtring/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace join_tostring_tochararray_subtring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //toString()
+
+            //string str1 = "Iqra";
+            //string str2 = "Tariq";
+            //Console.WriteLine(str1 = str2.ToString());
+            //Console.ReadKey();
+
+            //tochararray()
+
+            //string str = "IqraTariq";
+            //var chars=str.ToCharArray();
+            //for (int i = 0; i < chars.Length; i++)
+            //    Console.WriteLine("{0}:{1}", i, chars[i]);
+            //Console.ReadKey();
+          
+            //Console.ReadKey();
+
+            //substring()
+
+            //string str = "NaeemUrRehman";
+            //string str1 = str.Substring(7);
+            //string str2 = str.Substring(0, 7);
+
+            //Console.WriteLine("substring:{0}", str1, str2);
+            //Console.ReadKey();
+
+            
+        }
+    }
+}
