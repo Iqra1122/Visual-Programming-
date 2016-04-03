e107d5add13d99bbf4122cdd1f521812129f2c15
iqra1122 <iqra.tariq83@gmail.com>
2016-04-03 15:14:05 +0500

    duplicates

---
+++ duplicates/duplicates/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace duplicates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[] arr = { 1, 1, 4, 8, 7, 5, 5 };
+            //for(int i=0;i<arr.Length;i++)
+            //{
+            //    for(int j=i+1;j<arr.Length;j++)
+            //    {
+            //        if(arr[i]==arr[j])
+            //        {
+            //            Console.WriteLine(arr[i]);
+            //        }
+            //    }
+            //}
+            //Console.ReadKey();
+            //int[] arr1 = { 1, 2, 3, 4 };
+            //int[] arr2 = { 5, 6, 7, 8 };
+             
+            //for(int i=0;i<arr1.Length;i++)
+            //{
+            //    bool isnotpresent = false;
+            //    for(int j=0;j<arr2.Length;j++)
+            //    {
+            //        if(arr1[i]==arr2[j])
+            //        {
+            //            isnotpresent = true;
+            //        }
+            //    }
+            //    if(isnotpresent==true)
+            //    {
+            //        Console.WriteLine(arr1[i]);
+            //    }
+            //}
+            //Console.ReadKey();
+           
+        }
+    }
+}
