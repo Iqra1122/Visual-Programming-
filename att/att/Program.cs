0620b7cab6b37cd6e4f2ef84b4e56d43ffab3396
iqra1122 <iqra.tariq83@gmail.com>
2016-04-12 18:43:01 +0500

    attendance

---
+++ att/att/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace s.vp.lab4
+{
+    partial class Group
+    {
+        public void viewAttendens()
+        {
+            Console.WriteLine("\t attendens is : 100%");
+        }
+        public void Results()
+        {
+            Console.WriteLine("\t result is satisfactory");
+        }
+    }
+    partial class Group
+    {
+        public void Courses()
+        {
+            Console.WriteLine("courses are following\n1.SDA\n2.FMSE\n3.VP\n4.SAMD\n5.SQE");
+        }
+        public void info()
+        {
+            Console.WriteLine("\t Iam in BSE 6A");
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Group obj = new Group();
+            obj.Courses();
+            obj.info();
+            obj.Results();
+            obj.viewAttendens();
+            Console.ReadLine();
+        }
+    }
+}
+
+
