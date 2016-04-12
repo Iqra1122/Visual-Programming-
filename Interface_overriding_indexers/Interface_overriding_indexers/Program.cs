7694a3d4457e9366c0c3c7c321f57be7bfac3956
iqra1122 <iqra.tariq83@gmail.com>
2016-04-12 18:36:31 +0500

    volume of box

---
+++ Interface_overriding_indexers/Interface_overriding_indexers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classes
+{
+   class box
+   {
+       public double length;
+       public double breadth;
+       public double height;
+       public double volume(double length,double breadth,double height)
+       {
+           return length * breadth * height;
+       }
+   }
+    
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            box box1 = new box();
+            box1.length = 12;
+            box1.breadth = 13;
+            box1.height = 14;
+            Console.WriteLine("Volume of the box is: {0}", box1.volume(box1.length, box1.breadth, box1.height));
+            Console.ReadKey();
+
+        }
+    }
+}
