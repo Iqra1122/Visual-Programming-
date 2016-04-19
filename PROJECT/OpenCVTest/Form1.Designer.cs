025552500434f346b53da48a36031d3e5222dae5
iqra1122 <iqra.tariq83@gmail.com>
2016-04-19 21:13:19 +0500

    Semester Project

---
+++ PROJECT/OpenCVTest/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace OpenCVTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.imageName = new System.Windows.Forms.PictureBox();
+            this.imageName1 = new System.Windows.Forms.PictureBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.imageName)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageName1)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(28, 139);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(174, 31);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Select Image1";
+            // 
+            // imageName
+            // 
+            this.imageName.Location = new System.Drawing.Point(17, 21);
+            this.imageName.Name = "imageName";
+            this.imageName.Size = new System.Drawing.Size(383, 215);
+            this.imageName.TabIndex = 2;
+            this.imageName.TabStop = false;
+            // 
+            // imageName1
+            // 
+            this.imageName1.Location = new System.Drawing.Point(17, 21);
+            this.imageName1.Name = "imageName1";
+            this.imageName1.Size = new System.Drawing.Size(383, 207);
+            this.imageName1.TabIndex = 3;
+            this.imageName1.TabStop = false;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(448, 603);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(222, 45);
+            this.progressBar1.TabIndex = 18;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(28, 417);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(174, 31);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "Select Image2";
+            // 
+            // button3
+            // 
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button3.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(245, 603);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(197, 45);
+            this.button3.TabIndex = 16;
+            this.button3.Text = "Start identifying";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.imageName1);
+            this.panel2.Location = new System.Drawing.Point(245, 311);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(425, 250);
+            this.panel2.TabIndex = 15;
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.imageName);
+            this.panel1.Location = new System.Drawing.Point(245, 36);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(425, 253);
+            this.panel1.TabIndex = 14;
+            // 
+            // button2
+            // 
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(735, 403);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(139, 45);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Browse";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(735, 139);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(139, 45);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Browse";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1052, 707);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.imageName)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageName1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox imageName;
+        private System.Windows.Forms.PictureBox imageName1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+
+    }
+}
+
---
+++ PROJECT/OpenCVTest/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Emgu.CV;
+using Emgu.CV.UI;
+using Emgu.CV.Structure;
+
+
+namespace OpenCVTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string imagename;
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            {
+                try
+                {
+
+                    FileDialog fldlg = new OpenFileDialog();
+
+                    //specify your own initial directory
+
+                    fldlg.InitialDirectory = @":D\";
+
+                    //this will allow only those file extensions to be added
+
+                    fldlg.Filter = "Image File (*.jpg;*.bmp;*.gif;*.png)|*.jpg;*.bmp;*.gif;*.png";
+
+                    if (fldlg.ShowDialog() == DialogResult.OK)
+                    {
+
+                        imagename = fldlg.FileName;
+
+                        Bitmap newimg = new Bitmap(imagename);
+
+                        imageName.SizeMode = PictureBoxSizeMode.StretchImage;
+
+                        imageName.Image = (Image)newimg;
+
+                    }
+
+                    fldlg = null;
+
+                }
+
+                catch (System.ArgumentException ae)
+                {
+
+                    imagename = " ";
+
+                    MessageBox.Show(ae.Message.ToString());
+
+                }
+
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            {
+                try
+                {
+
+                    FileDialog fldlg = new OpenFileDialog();
+
+                    //specify your own initial directory
+
+                    fldlg.InitialDirectory = @":D\";
+
+                    //this will allow only those file extensions to be added
+
+                    fldlg.Filter = "Image File (*.jpg;*.bmp;*.gif;*.png)|*.jpg;*.bmp;*.gif;*.png";
+
+                    if (fldlg.ShowDialog() == DialogResult.OK)
+                    {
+
+                        imagename = fldlg.FileName;
+
+                        Bitmap newimg = new Bitmap(imagename);
+
+                        imageName1.SizeMode = PictureBoxSizeMode.StretchImage;
+
+                        imageName1.Image = (Image)newimg;
+
+                    }
+
+                    fldlg = null;
+
+                }
+
+                catch (System.ArgumentException ae)
+                {
+
+                    imagename = " ";
+
+                    MessageBox.Show(ae.Message.ToString());
+
+                }
+
+
+            }
+        }
+    }
+}
