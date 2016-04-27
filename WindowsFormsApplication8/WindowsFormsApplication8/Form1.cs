6568a6586ca1de482bc8cc47a5bbf8f345a69b4c
iqra1122 <iqra.tariq83@gmail.com>
2016-04-27 22:13:46 +0500

    form...

---
+++ WindowsFormsApplication8/WindowsFormsApplication8/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication8
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private const int max = 5;
+        string[] student = new string[max];
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            //student[0] = t1.Text;
+            //student[1] = t2.Text;
+            //student[2] = t3.Text;
+
+           MessageBox.Show("Data is saved successfully");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StringBuilder t1 = new StringBuilder();
+            t1.AppendLine(student [0]);
+            t1.AppendLine(student[1]);
+            t1.AppendLine(student[2]);
+            MessageBox.Show(t1.ToString(), "data is");
+
+        }
+    }
+}
