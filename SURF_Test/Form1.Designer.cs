651f01fe60f899b1f90243c7685750d55c6bc526
iqra.tariq83 <iqra.tariq83@gmail.com>
2016-05-05 06:33:32 -0700

    my project

---
+++ SURF_Test/Form1.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace SURF_Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.imageBox1 = new Emgu.CV.UI.ImageBox();
+            this.tbSrcImage = new System.Windows.Forms.TextBox();
+            this.tbFndImage = new System.Windows.Forms.TextBox();
+            this.btnSrcImage = new System.Windows.Forms.Button();
+            this.btnFndImage = new System.Windows.Forms.Button();
+            this.ofdSrcImage = new System.Windows.Forms.OpenFileDialog();
+            this.ofdFndImage = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pbSource = new System.Windows.Forms.PictureBox();
+            this.pbFnd = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbFnd)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(548, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 71);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Perform Surf";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // imageBox1
+            // 
+            this.imageBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.imageBox1.Location = new System.Drawing.Point(312, 102);
+            this.imageBox1.Name = "imageBox1";
+            this.imageBox1.Size = new System.Drawing.Size(906, 476);
+            this.imageBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imageBox1.TabIndex = 2;
+            this.imageBox1.TabStop = false;
+            // 
+            // tbSrcImage
+            // 
+            this.tbSrcImage.Location = new System.Drawing.Point(105, 28);
+            this.tbSrcImage.Name = "tbSrcImage";
+            this.tbSrcImage.Size = new System.Drawing.Size(377, 20);
+            this.tbSrcImage.TabIndex = 3;
+            // 
+            // tbFndImage
+            // 
+            this.tbFndImage.Location = new System.Drawing.Point(105, 55);
+            this.tbFndImage.Name = "tbFndImage";
+            this.tbFndImage.Size = new System.Drawing.Size(377, 20);
+            this.tbFndImage.TabIndex = 4;
+            // 
+            // btnSrcImage
+            // 
+            this.btnSrcImage.Location = new System.Drawing.Point(488, 26);
+            this.btnSrcImage.Name = "btnSrcImage";
+            this.btnSrcImage.Size = new System.Drawing.Size(27, 23);
+            this.btnSrcImage.TabIndex = 5;
+            this.btnSrcImage.Text = "...";
+            this.btnSrcImage.UseVisualStyleBackColor = true;
+            this.btnSrcImage.Click += new System.EventHandler(this.btnSrcImage_Click);
+            // 
+            // btnFndImage
+            // 
+            this.btnFndImage.Location = new System.Drawing.Point(488, 53);
+            this.btnFndImage.Name = "btnFndImage";
+            this.btnFndImage.Size = new System.Drawing.Size(27, 23);
+            this.btnFndImage.TabIndex = 6;
+            this.btnFndImage.Text = "...";
+            this.btnFndImage.UseVisualStyleBackColor = true;
+            this.btnFndImage.Click += new System.EventHandler(this.btnFndImage_Click);
+            // 
+            // ofdSrcImage
+            // 
+            this.ofdSrcImage.FileName = "openFileDialog1";
+            // 
+            // ofdFndImage
+            // 
+            this.ofdFndImage.FileName = "openFileDialog2";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Model Image";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Image To Track";
+            // 
+            // pbSource
+            // 
+            this.pbSource.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pbSource.Location = new System.Drawing.Point(20, 102);
+            this.pbSource.Name = "pbSource";
+            this.pbSource.Size = new System.Drawing.Size(263, 217);
+            this.pbSource.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbSource.TabIndex = 9;
+            this.pbSource.TabStop = false;
+            // 
+            // pbFnd
+            // 
+            this.pbFnd.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pbFnd.Location = new System.Drawing.Point(20, 338);
+            this.pbFnd.Name = "pbFnd";
+            this.pbFnd.Size = new System.Drawing.Size(263, 230);
+            this.pbFnd.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbFnd.TabIndex = 10;
+            this.pbFnd.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(17, 86);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Source Image";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(17, 322);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(72, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Image To find";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(309, 86);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(69, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "SURF Result";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1256, 602);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pbFnd);
+            this.Controls.Add(this.pbSource);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnFndImage);
+            this.Controls.Add(this.btnSrcImage);
+            this.Controls.Add(this.tbFndImage);
+            this.Controls.Add(this.tbSrcImage);
+            this.Controls.Add(this.imageBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Channel Logo Finder";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbFnd)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private Emgu.CV.UI.ImageBox imageBox1;
+        private System.Windows.Forms.TextBox tbSrcImage;
+        private System.Windows.Forms.TextBox tbFndImage;
+        private System.Windows.Forms.Button btnSrcImage;
+        private System.Windows.Forms.Button btnFndImage;
+        private System.Windows.Forms.OpenFileDialog ofdSrcImage;
+        private System.Windows.Forms.OpenFileDialog ofdFndImage;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pbSource;
+        private System.Windows.Forms.PictureBox pbFnd;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ SURF_Test/Form1.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Windows.Forms;
+using Emgu.CV;
+
+using Emgu.CV.CvEnum;
+using Emgu.CV.Features2D;
+using Emgu.CV.Structure;
+using Emgu.CV.Util;
+#if !__IOS__
+using Emgu.CV.Cuda;
+#endif
+using Emgu.CV.XFeatures2D;
+
+namespace SURF_Test
+{
+    public partial class Form1 : Form
+    {
+        PerformSURF surf = new PerformSURF();
+        DialogResult dr;
+        public int uniqueness;
+        public int hesianThresh;
+        public Form1()
+        {
+            InitializeComponent();
+            dr = new DialogResult();
+        }
+
+     
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            Image<Bgr, byte> src = new Image<Bgr, byte>(tbFndImage.Text.ToString());
+            Image<Bgr, byte> fnd = new Image<Bgr, byte>(tbSrcImage.Text.ToString());
+            Mat srcImg = new Mat();
+            srcImg = src.Mat;
+            Mat fndImg = new Mat();
+            fndImg = fnd.Mat;
+            long a = 1000;
+
+            imageBox1.Image = surf.Draw(srcImg, fndImg, out a);
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            //Image<Bgr, byte> src = new Image<Bgr, byte>(SURF_Test.Properties.Resources.src);
+            //Image<Bgr, byte> fnd = new Image<Bgr, byte>(SURF_Test.Properties.Resources.fnd);
+
+            //Mat srcImg = new Mat();
+            //srcImg = src.Mat;
+            //Mat fndImg = new Mat();
+            //fndImg = src.Mat;
+            //long a = 10000;
+            //Draw(srcImg, fndImg, out a);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSrcImage_Click(object sender, EventArgs e)
+        {
+            ofdSrcImage.ShowDialog();
+            tbSrcImage.Text = ofdSrcImage.FileName.ToString();
+            Image imgSrc = Image.FromFile(tbSrcImage.Text.ToString());
+            pbSource.Image = imgSrc ;
+
+           
+        }
+
+        private void btnFndImage_Click(object sender, EventArgs e)
+        {
+            ofdFndImage.ShowDialog();
+            tbFndImage.Text = ofdFndImage.FileName.ToString();
+            Image imgFnd = Image.FromFile(tbFndImage.Text.ToString());
+            pbFnd.Image = imgFnd;
+        }
+    }
+
+    public class PerformSURF : Form
+    {
+        public static void FindMatch(Mat modelImage, Mat observedImage, out long matchTime, out VectorOfKeyPoint modelKeyPoints, out VectorOfKeyPoint observedKeyPoints, VectorOfVectorOfDMatch matches, out Mat mask, out Mat homography)
+        {
+            int k = 2;
+            //Form obj = new Form();
+            //Form1 obj1 = new Form1();
+            //obj1.tbUni
+            double uniquenessThreshold = 0.3;
+            double hessianThresh = 340;
+
+            Stopwatch watch;
+            homography = null;
+
+            modelKeyPoints = new VectorOfKeyPoint();
+            observedKeyPoints = new VectorOfKeyPoint();
+
+#if !__IOS__
+            if (CudaInvoke.HasCuda)
+            {
+                CudaSURF surfCuda = new CudaSURF((float)hessianThresh);
+                using (GpuMat gpuModelImage = new GpuMat(modelImage))
+                //extract features from the object image
+                using (GpuMat gpuModelKeyPoints = surfCuda.DetectKeyPointsRaw(gpuModelImage, null))
+                using (GpuMat gpuModelDescriptors = surfCuda.ComputeDescriptorsRaw(gpuModelImage, null, gpuModelKeyPoints))
+                using (CudaBFMatcher matcher = new CudaBFMatcher(DistanceType.L2))
+                {
+                    surfCuda.DownloadKeypoints(gpuModelKeyPoints, modelKeyPoints);
+                    watch = Stopwatch.StartNew();
+
+                    // extract features from the observed image
+                    using (GpuMat gpuObservedImage = new GpuMat(observedImage))
+                    using (GpuMat gpuObservedKeyPoints = surfCuda.DetectKeyPointsRaw(gpuObservedImage, null))
+                    using (GpuMat gpuObservedDescriptors = surfCuda.ComputeDescriptorsRaw(gpuObservedImage, null, gpuObservedKeyPoints))
+                    //using (GpuMat tmp = new GpuMat())
+                    //using (Stream stream = new Stream())
+                    {
+                        matcher.KnnMatch(gpuObservedDescriptors, gpuModelDescriptors, matches, k);
+
+                        surfCuda.DownloadKeypoints(gpuObservedKeyPoints, observedKeyPoints);
+
+                        mask = new Mat(matches.Size, 1, DepthType.Cv8U, 1);
+                        mask.SetTo(new MCvScalar(255));
+                        Features2DToolbox.VoteForUniqueness(matches, uniquenessThreshold, mask);
+
+                        int nonZeroCount = CvInvoke.CountNonZero(mask);
+                        if (nonZeroCount >= 4)
+                        {
+                            nonZeroCount = Features2DToolbox.VoteForSizeAndOrientation(modelKeyPoints, observedKeyPoints,
+                               matches, mask, 1.5, 20);
+                            if (nonZeroCount >= 4)
+                                homography = Features2DToolbox.GetHomographyMatrixFromMatchedFeatures(modelKeyPoints,
+                                   observedKeyPoints, matches, mask, 2);
+                        }
+                    }
+                    watch.Stop();
+                }
+            }
+            else
+#endif
+            {
+                using (UMat uModelImage = modelImage.ToUMat(AccessType.Read))
+                using (UMat uObservedImage = observedImage.ToUMat(AccessType.Read))
+                {
+                    SURF surfCPU = new SURF(hessianThresh);
+                    //extract features from the object image
+                    UMat modelDescriptors = new UMat();
+                    surfCPU.DetectAndCompute(uModelImage, null, modelKeyPoints, modelDescriptors, false);
+                    watch = Stopwatch.StartNew();
+                    // extract features from the observed image
+                    UMat observedDescriptors = new UMat();
+                    surfCPU.DetectAndCompute(uObservedImage, null, observedKeyPoints, observedDescriptors, false);
+                    BFMatcher matcher = new BFMatcher(DistanceType.L2);
+                    matcher.Add(modelDescriptors);
+
+                    matcher.KnnMatch(observedDescriptors, matches, k, null);
+                    mask = new Mat(matches.Size, 1, DepthType.Cv8U, 1);
+                    mask.SetTo(new MCvScalar(255));
+                    Features2DToolbox.VoteForUniqueness(matches, uniquenessThreshold, mask);
+
+                    int nonZeroCount = CvInvoke.CountNonZero(mask);
+                    if (nonZeroCount >= 4)
+                    {
+                        nonZeroCount = Features2DToolbox.VoteForSizeAndOrientation(modelKeyPoints, observedKeyPoints,
+                           matches, mask, 1.5, 20);
+                        if (nonZeroCount >= 4)
+                            homography = Features2DToolbox.GetHomographyMatrixFromMatchedFeatures(modelKeyPoints,
+                               observedKeyPoints, matches, mask, 2);
+                    }
+
+                    watch.Stop();
+                }
+            }
+            matchTime = watch.ElapsedMilliseconds;
+        }
+
+        /// <summary>
+        /// Draw the model image and observed image, the matched features and homography projection.
+        /// </summary>
+        /// <param name="modelImage">The model image</param>
+        /// <param name="observedImage">The observed image</param>
+        /// <param name="matchTime">The output total time for computing the homography matrix.</param>
+        /// <returns>The model image and observed image, the matched features and homography projection.</returns>
+        public Mat Draw(Mat modelImage, Mat observedImage, out long matchTime)
+        {
+            Mat homography;
+            VectorOfKeyPoint modelKeyPoints;
+            VectorOfKeyPoint observedKeyPoints;
+            using (VectorOfVectorOfDMatch matches = new VectorOfVectorOfDMatch())
+            {
+                Mat mask;
+                FindMatch(modelImage, observedImage, out matchTime, out modelKeyPoints, out observedKeyPoints, matches,
+                   out mask, out homography);
+
+                //Draw the matched keypoints
+                Mat result = new Mat();
+                Features2DToolbox.DrawMatches(modelImage, modelKeyPoints, observedImage, observedKeyPoints,
+                   matches, result, new MCvScalar(255, 255, 255), new MCvScalar(255, 255, 255), mask);
+
+                #region draw the projected region on the image
+
+                if (homography != null)
+                {
+                    //draw a rectangle along the projected model
+                    Rectangle rect = new Rectangle(Point.Empty, modelImage.Size);
+                    PointF[] pts = new PointF[]
+                    {
+                          new PointF(rect.Left, rect.Bottom),
+                          new PointF(rect.Right, rect.Bottom),
+                          new PointF(rect.Right, rect.Top),
+                          new PointF(rect.Left, rect.Top)
+                    };
+                    pts = CvInvoke.PerspectiveTransform(pts, homography);
+
+                    Point[] points = Array.ConvertAll<PointF, Point>(pts, Point.Round);
+                    using (VectorOfPoint vp = new VectorOfPoint(points))
+                    {
+                        CvInvoke.Polylines(result, vp, true, new MCvScalar(255, 0, 0, 255), 5);
+                    }
+
+                }
+
+                #endregion
+
+                return result;
+
+            }
+        }
+    }
+
+
+
+   
+    
+}
